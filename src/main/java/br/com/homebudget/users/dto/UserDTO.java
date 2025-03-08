5aafd0086e10a955ed006ad4ee5a9a35877009ee
Antonio Vieira <vieira.n55@gmail.com>
2025-03-08 10:58:18 -0300

    first commit + CRUD User ok

---
+++ src/main/java/br/com/homebudget/users/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package br.com.homebudget.users.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public record UserDTO(
+        Long id,
+        String name,
+        String email,
+        String password
+) {
+}
